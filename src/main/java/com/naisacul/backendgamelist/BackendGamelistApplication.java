fb40bda444b60fd5c1fe8fd5a7a208c984cafdd7
NailSacul <ianlucaschaves@gmail.com>
2023-11-28 01:40:20 -0300

    Project created

---
+++ src/main/java/com/naisacul/backendgamelist/BackendGamelistApplication.java
@@ -0,0 +1,13 @@
+package com.naisacul.backendgamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGamelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naisacul/backendgamelist/BackendGamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naisacul.backendgamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
