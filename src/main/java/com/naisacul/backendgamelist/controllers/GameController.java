9303aa36a1b2edb1ce056c921388d8558510f9f3
NailSacul <ianlucaschaves@gmail.com>
2023-11-28 11:41:49 -0300

    Controllers

---
+++ src/main/java/com/naisacul/backendgamelist/controllers/GameController.java
@@ -0,0 +1,32 @@
+package com.naisacul.backendgamelist.controllers;
+
+import com.naisacul.backendgamelist.dtos.GameDTO;
+import com.naisacul.backendgamelist.dtos.GameMinDTO;
+import com.naisacul.backendgamelist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+
+    @GetMapping
+    public List<GameMinDTO> findAll(){
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
