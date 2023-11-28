733f4be3d171a5d60b9be2ffb7f3efce818b7003
NailSacul <ianlucaschaves@gmail.com>
2023-11-28 11:39:47 -0300

    Services

---
+++ src/main/java/com/naisacul/backendgamelist/services/GameListService.java
@@ -0,0 +1,43 @@
+package com.naisacul.backendgamelist.services;
+
+import com.naisacul.backendgamelist.dtos.GameListDTO;
+import com.naisacul.backendgamelist.entities.GameList;
+import com.naisacul.backendgamelist.projections.GameMinProjection;
+import com.naisacul.backendgamelist.repositories.GameListRepository;
+import com.naisacul.backendgamelist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+    }
+}
---
+++ src/main/java/com/naisacul/backendgamelist/services/GameService.java
@@ -0,0 +1,37 @@
+package com.naisacul.backendgamelist.services;
+
+import com.naisacul.backendgamelist.dtos.GameDTO;
+import com.naisacul.backendgamelist.dtos.GameMinDTO;
+import com.naisacul.backendgamelist.entities.Game;
+import com.naisacul.backendgamelist.projections.GameMinProjection;
+import com.naisacul.backendgamelist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findAll(){
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+}
